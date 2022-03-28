7644667c1d92b682f04232e319b19319fe49e69c
Kayl Guise <kaylee.guise@gmail.com>
2022-03-28 13:20:08 -0500

    Changed arrangement of if/else statements and category arrays.

--- daytrip-script.js
+++ daytrip-script.js
@@ -4,24 +4,6 @@
 
 let locations = ["Wisconsin Dells", "Milwaukee", "Eagle", "Baraboo"];
 
-//FOOD/TRANSPORT/FUN ARRAYS
-  
-let dellsFood = ["Moose Jaw Pizza", "Monk's Bar & Grill", "High Rock Cafe"]
-let dellsTransport = ["Dells City Taxi", "Original Wisconsin Ducks", "Dells Boat Tours"]
-let dellsFun = ["Noah's Ark Waterpark", "Tommy Bartlett Exploratory", "Wizard Quest"]
-
-let mkeFood = ["Mader's Restaurant", "Blue's Egg", "AJ Bombers"]
-let mkeTransport = ["Milwaukee County Transit System", "Olaa Taxi Milwaukee", "Enterprise Rent-A-Car"]
-let mkeFun = ["Milwaukee County Zoo", "Pabst Mansion", "Mitchell Park Horticultural Conservatory"]
-
-let eagleFood = ["Knucklehead Pub", "Hen House Cafe", "Fork in the Road"]
-let eagleTransport = ["East Troy Trolley", "Wild 3L Ranch", "Yellow Discount Cab"]
-let eagleFun = ["Old World Wisconsin", "Paradise Springs Nature Trail", "Kettle Moraine Ranch"]
-
-let barabooFood = ["Log Cabin Family Restaurant", "Cheeze Factory Restaurant", "Baraboo Burger Company"]
-let barabooTransport = ["Lamers Bus Lines", "Greyhound Bus", "Kobussen Buses, Ltd."]
-let barabooFun = ["International Crane Foundation", "Circus World Museum", "Devil's Lake State Park"]
-
 //DESTINATION FUNCTION
 
 function pickTrip(randomLocation) {
@@ -36,41 +18,53 @@ function pickTrip(randomLocation) {
 
 function pickRestaurant(foodArray) {
     let i = Math.floor(Math.random() * foodArray.length);
-    return (foodArray[i]);
+    console.log(foodArray[i]);
 }
 
 function pickTransportation(transportArray) {
     let i = Math.floor(Math.random() * transportArray.length);
-    return (transportArray[i]);
+    console.log(transportArray[i]);
 }
 
 function pickAttraction(funArray) {
-    let i = Math.floor(Math.random() * funArray);
-    return (funArray[i]);
+    let i = Math.floor(Math.random() * funArray.length);
+    console.log(funArray[i]);
 }
 
-//IF-ELSE STATEMENTS
+//FOOD, TRANSPORT, FUN PICKER
 
-if(chosenLocale === "Wisconsin Dells"); {
-    let food = dellsFood;
-    let transport = dellsTransport;
-    let fun = dellsFun;
-}
-if(chosenLocale === "Milwaukee"); {
-    let food = dellsFood;
-    let transport = dellsTransport;
-    let fun = dellsFun;
-}
-if(chosenLocale === "Eagle"); {
-    let food = dellsFood;
-    let transport = dellsTransport;
-    let fun = dellsFun;
-}
-if(chosenLocale === "Baraboo");{
-    let food = dellsFood;
-    let transport = dellsTransport;
-    let fun = dellsFun;
+if (chosenLocale === "Wisconsin Dells") {
+let food = ["Moose Jaw Pizza", "Monk's Bar & Grill", "High Rock Cafe"];
+let transport = ["Dells City Taxi", "Original Wisconsin Ducks", "Dells Boat Tours"];
+let fun = ["Noah's Ark Waterpark", "Tommy Bartlett Exploratory", "Wizard Quest"];
+pickRestaurant(food);
+pickTransportation(transport);
+pickAttraction(fun);
 }
 
-//Question 1: I thought there was a way to make the declaration and return of an index even more concise than 2 lines. Is there one?
-//Question 2: Do I need a for loop containing a .find method for each of the functions so the computer knows from which arrays to grab from?
+if (chosenLocale === "Milwaukee") {
+let food = ["Mader's Restaurant", "Blue's Egg", "AJ Bombers"];
+let transport = ["Milwaukee County Transit System", "Olaa Taxi Milwaukee", "Enterprise Rent-A-Car"];
+let fun = ["Milwaukee County Zoo", "Pabst Mansion", "Mitchell Park Horticultural Conservatory"];
+pickRestaurant(food);
+pickTransportation(transport);
+pickAttraction(fun);
+}
+
+if (chosenLocale === "Eagle") {
+let food = ["Knucklehead Pub", "Hen House Cafe", "Fork in the Road"];
+let transport = ["East Troy Trolley", "Wild 3L Ranch", "Yellow Discount Cab"];
+let fun = ["Old World Wisconsin", "Paradise Springs Nature Trail", "Kettle Moraine Ranch"];
+pickRestaurant(food);
+pickTransportation(transport);
+pickAttraction(fun);
+}
+
+if (chosenLocale === "Baraboo") {
+let food = ["Log Cabin Family Restaurant", "Cheeze Factory Restaurant", "Baraboo Burger Company"];
+let transport = ["Lamers Bus Lines", "Greyhound Bus", "Kobussen Buses, Ltd."];
+let fun = ["International Crane Foundation", "Circus World Museum", "Devil's Lake State Park"];
+pickRestaurant(food);
+pickTransportation(transport);
+pickAttraction(fun);
+}
