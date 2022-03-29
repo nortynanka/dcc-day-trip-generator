38fbbd356f0f6dba0cec7b99344cb55866c23d0e
Kayl Guise <kaylee.guise@gmail.com>
2022-03-29 10:09:29 -0500

    Submission for project grade

--- daytrip-script.js
+++ daytrip-script.js
@@ -1,84 +1,49 @@
 "use strict"
 
+//OPTIONS
 
-//LOCATION CHOICES
-
-let locationsArray = ["Wisconsin Dells", "Milwaukee", "Eagle", "Baraboo"];
+let location = ["Wisconsin Dells", "Milwaukee", "Eagle", "Baraboo"];
 let food = ["Moose Jaw Pizza", "Monk's Bar & Grill", "High Rock Cafe"];
 let transport = ["Dells City Taxi", "Original Wisconsin Ducks", "Dells Boat Tours"];
 let fun = ["Noah's Ark Waterpark", "Tommy Bartlett Exploratory", "Wizard Quest"];
 
-let selectedDestination = pickTrip(locationsArray); {
+//LOCATION
 
-let userConfirmation = confirmTripChoice(selectedDestination);
+let selectedDestination = pickThing(location);
 
-while (userConfirmation == "N") {
-    selectedDestination = pickTrip(locationsArray);
-    console.log("You will be going on a day trip to " + selectedDestination);
-    userConfirmation = confirmTripChoice(selectedDestination);
-    }
-}
+userConfirmation = confirmTripChoice(selectedDestination);
 
-let selectedRestaurant = pickRestaurant(food); {
+pickSomethingElse();
 
-let userConfirmation = confirmTripChoice(selectedRestaurant); 
+//RESTAURANT
 
-while(userConfirmation == "N") {
-    selectedRestaurant = pickRestaurant(food);
-    console.log("You will be eating at " + selectedRestaurant);
-    userConfirmation = confirmTripChoice(selectedRestaurant);
-    }
-}
+let selectedRestaurant = pickThing(food); 
 
-let selectedTransportation = pickTransportation(transport); {
+userConfirmation = confirmTripChoice(selectedRestaurant); 
 
-let userConfirmation = confirmTripChoice(selectedTransportation);
+pickSomethingElse();
 
-while(userConfirmation == "N") {
-    selectedTransportation = pickTransportation(transport);
-    console.log("You will travel via " + selectedTransportation);
-    userConfirmation = confirmTripChoice(selectedTransportation);
-    }
-}  
+//TRANSPORTATION
+    
+let selectedTransportation = pickThing(transport); 
 
-let selectedEntertainment = pickAttraction(fun); {
+userConfirmation = confirmTripChoice(selectedTransportation);
 
-let userConfirmation = confirmTripChoice(selectedEntertainment);
+pickSomethingElse();
 
-while(userConfirmation == "N") {
-    selectedEntertainment = pickAttraction(fun);
-    console.log("You will go to " + selectedEntertainment);
-    userConfirmation = confirmTripChoice(selectedEntertainment);
-    }
-}
+//ENTERTAINMENT
 
-//TRIP CONFIRMATION
+let selectedEntertainment = pickThing(fun); 
 
-if (userConfirmation === "Y") {
-alert("Yay! Enjoy your trip!");
-console.log(`You are going on trip to ${selectedDestination}. You will eat a meal at ${selectedRestaurant}, travel via ${selectedTransportation}, and visit ${selectedEntertainment}.`)
-}
+userConfirmation = confirmTripChoice(selectedEntertainment);
 
-//FOOD, TRANSPORT, FUN, DESTINATION FUNCTIONS
+pickSomethingElse();
 
-function pickTrip(randomLocation) {
-    let i = Math.floor(Math.random() * randomLocation.length);
-    return (randomLocation[i]);
-}
+//FUNCTIONS
 
-function pickRestaurant(foodArray) {
-    let i = Math.floor(Math.random() * foodArray.length);
-    return (foodArray[i]);
-}
-
-function pickTransportation(transportArray) {
-    let i = Math.floor(Math.random() * transportArray.length);
-    return (transportArray[i]);
-}
-
-function pickAttraction(funArray) {
-    let i = Math.floor(Math.random() * funArray.length);
-    return (funArray[i]);
+function pickThing(someArray) {
+    let i = Math.floor(Math.random() * someArray.length);
+    return (someArray[i]);
 }
 
 function confirmTripChoice(option) {
@@ -86,47 +51,7 @@ function confirmTripChoice(option) {
     return userConfirmation;
 }
 
-
-
-//FOOD, TRANSPORT, FUN PICKER
-
-// let chosenLocale = pickTrip(locationsArray);
-//     console.log(`You will be going on a day trip to ${chosenLocale}.`);
-
-// if (chosenLocale === "Wisconsin Dells") {
-// let food = ["Moose Jaw Pizza", "Monk's Bar & Grill", "High Rock Cafe"];
-// let transport = ["Dells City Taxi", "Original Wisconsin Ducks", "Dells Boat Tours"];
-// let fun = ["Noah's Ark Waterpark", "Tommy Bartlett Exploratory", "Wizard Quest"];
-// console.log("You will be eating at " + pickRestaurant(food));
-// console.log("You will travel via " + pickTransportation(transport));
-// console.log("You will go to " + pickAttraction(fun));
-// }
-
-// if (chosenLocale === "Milwaukee") {
-// let food = ["Mader's Restaurant", "Blue's Egg", "AJ Bombers"];
-// let transport = ["Milwaukee County Transit System", "Olaa Taxi Milwaukee", "Enterprise Rent-A-Car"];
-// let fun = ["Milwaukee County Zoo", "Pabst Mansion", "Mitchell Park Horticultural Conservatory"];
-// console.log("You will be eating at " + pickRestaurant(food));
-// console.log("You will travel via " + pickTransportation(transport));
-// console.log("You will go to " + pickAttraction(fun));
-// }
-
-// if (chosenLocale === "Eagle") {
-// let food = ["Knucklehead Pub", "Hen House Cafe", "Fork in the Road"];
-// let transport = ["East Troy Trolley", "Wild 3L Ranch", "Yellow Discount Cab"];
-// let fun = ["Old World Wisconsin", "Paradise Springs Nature Trail", "Kettle Moraine Ranch"];
-// console.log("You will be eating at " + pickRestaurant(food));
-// console.log("You will travel via " + pickTransportation(transport));
-// console.log("You will go to " + pickAttraction(fun));
-// }
-
-// if (chosenLocale === "Baraboo") {
-// let food = ["Log Cabin Family Restaurant", "Cheeze Factory Restaurant", "Baraboo Burger Company"];
-// let transport = ["Lamers Bus Lines", "Greyhound Bus", "Kobussen Buses, Ltd."];
-// let fun = ["International Crane Foundation", "Circus World Museum", "Devil's Lake State Park"];
-// console.log("You will be eating at " + pickRestaurant(food));
-// console.log("You will travel via " + pickTransportation(transport));
-// console.log("You will go to " + pickAttraction(fun));
-// }
-
-//USER CONFIRMATION
+function chooseAgain(categoryName) {
+    while(userConfirmation == "N");
+        pickThing(option);
+}
